2d87fde22e0303317ec2a20139a1d992f227e8fc
zfsilverfox <47052123+zfsilverfox@users.noreply.github.com>
2019-01-26 18:04:12 +0900

    Migan Shot Shori
    MiganShotShori

---
+++ Assets/Scripts/MiganTHrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiganTHrow : MonoBehaviour
+{
+
+
+    Rigidbody MiganRigid;   //蜜柑の自身RigidBody
+
+    [SerializeField] float Force=4f;
+
+
+
+    [SerializeField] float TimeToDestroy = 4f;
+
+    private void Start()
+    {
+        MiganRigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(0.0f, 0.0f, Force * Time.deltaTime));
+        Destroy(this.gameObject,TimeToDestroy);
+
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/ShotInstantiate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotInstantiate : MonoBehaviour
+{
+    public GameObject MiganInstiante;
+
+
+
+    [SerializeField] float WaitForCLicked = 2f;
+
+    bool IfClicked = false;
+
+    void Start()
+    {
+        IfClicked = false;
+    }
+
+
+  void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(IFCLICKEDBYPLAYER());
+
+            //プレイヤーが押さしと、二つ弾を逃げるようにする。
+            //If The Player Click, To Avoid More than One Shot Been Appeared
+            if (IfClicked)
+            {
+                Instantiate(MiganInstiante, transform.position, Quaternion.identity);
+            }
+
+        }
+          
+
+        
+    }
+ 
+
+
+
+    IEnumerator IFCLICKEDBYPLAYER()
+    {
+
+        IfClicked = true;
+        yield return new WaitForSeconds(WaitForCLicked);
+        IfClicked = false;
+
+    }
+
+}
