7cb9e5fa953c09cd8b0597767563c48e90b42e1e
Aaron Hilton <aaron@steampunk.digital>
2019-01-26 17:40:01 +0900

    Framing the initial game architecture.

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public static Main instance = null;
+
+    void Awake() {
+        if( instance == null ) {
+            instance = this;
+        }
+        else if( instance != this ) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        InitGame();
+    }
+
+    // Init all the working parts of the game.
+    void InitGame() {
+
+    }
+
+    // Update the game state every frame.
+    void Update() {
+    }
+}
